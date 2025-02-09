3408b4009b79f57142b86421a21089a9c0c262d4
Nijat Nasirli <nicat.nasirli0511@gmail.com>
2025-02-09 14:54:00 +0400

    Majority element algorithm

--- src/algorithms/leetcode/topInterview150/MajorityElement.java
+++ src/algorithms/leetcode/topInterview150/MajorityElement.java
@@ -1,29 +1,20 @@
 package algorithms.leetcode.topInterview150;
 
+import java.util.Arrays;
+
 //Given an array nums of size n, return the majority element.
 //
 //The majority element is the element that appears more than ⌊n / 2⌋ times.
 // You may assume that the majority element always exists in the array.
 public class MajorityElement {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 2, 2};
-        System.out.println(majorityElement(array));
+        int[] nums = {1, 2, 3, 2, 2};
+        System.out.println(majorityElement(nums));
     }
 
-    public static int majorityElement(int[] array) {
-        int arrayLength = array.length;
-        int count;
-        for (int i = 0; i < arrayLength; i++) {
-            count = 0;
-            for (int j = 0; j < arrayLength; j++) {
-                if (array[i] == array[j]) {
-                    count++;
-                }
-            }
-            if (count > arrayLength / 2) {
-                return array[i];
-            }
-        }
-        return -1;
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);//(1,2,2,2,3)
+        return nums[nums.length / 2];
     }
+
 }
