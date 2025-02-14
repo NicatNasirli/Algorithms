345612519b73ad7dbe150a3031602b32ebd695b4
Nijat Nasirli <nicat.nasirli0511@gmail.com>
2025-02-14 23:52:31 +0400

    Two Sum 2 Input Array Is Sorted algorithm

---
+++ src/algorithms/leetcode/topInterview150/TwoSum2InputArrayIsSorted.java
@@ -0,0 +1,34 @@
+package algorithms.leetcode.topInterview150;
+
+import java.util.Arrays;
+
+public class TwoSum2InputArrayIsSorted {
+
+//    Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+//    find two numbers such that they add up to a specific target number.
+//    Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+//
+//    Return the indices of the two numbers, index1 and index2,
+//    added by one as an integer array [index1, index2] of length 2.
+//
+//    The tests are generated such that there is exactly one solution. You may not use the same element twice.
+//
+//    Your solution must use only constant extra space.
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 3, 4};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(numbers, target)));
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) return new int[] {left + 1, right + 1};
+            else if (sum < target) left++;
+            else right--;
+        }
+        return null;
+    }
+}
